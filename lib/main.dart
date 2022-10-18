e64bbdf18bce64ccbff21feb1cdc189fc8c7c71c
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-10-18 23:04:06 +0300

    adding app files

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'views/screens/home_screen.dart';
+
+
+void main() {
+  runApp( const MaterialApp(
+    home:HomeScreen(),
+  ));
+}
+
+
+
